f5351259c8f1d8ebb73e64d49af17b029bec2589
bieganskip1996 <bieganski1996@gmail.com>
2025-04-29 09:28:39 +0200

    Add icon display grid to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,19 @@
-rfc
+import * as Icons from "@/components/icons/icons";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      {Object.entries(Icons).map(([key, Icon]) => (
+        <div
+          key={key}
+          className="flex flex-col items-center p-4 border rounded-lg"
+        >
+          <div className="mb-2 size-10">
+            <Icon />
+          </div>
+          <span className="text-sm text-gray-600">{key}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
